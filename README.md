f400d71ff78748648a0cbf527cd31d7dae65fd7c
fwang1@us.ibm.com <fwang1@us.ibm.com>
2015-05-27 16:35:51 -0700

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# node-red-contrib-kafka-node
+# Kafka Node
+Kafka node can produce/consume the mesasges to/from kafka cluster along with topic(s) on [NodeRED].
+
+
+# Install
+
+Install from [npm](http://npmjs.org)
+```
+npm install -g node-red-contrib-kafka-node
+```
+
+# Prerequisites
+ * Node-RED platform. (see [Node-RED](http://nodered.org/docs/getting-started/installation.html))
+ * Kafka cluster (see [Kafka](http://kafka.apache.org/documentation.html#gettingStarted)). 
+ * Some topics (see [how to create a topic](http://kafka.apache.org/documentation.html#quickstart))
+
+# Usage
+##### Producer Node
+- Parameters:
+ - Zookeeper Quarum
+ - Topics
+
+##### Consumer Node
+- Parameters:
+ - Zookeeper Quarum
+ - Topics
+ - GroupId
+
+
+# Version
+0.1.0
+
+# Tech
+
+ * [Kafka] - Apache Kafka is publish-subscribe messaging rethought as a distributed commit log.
+ * [Zookeeper] - ZooKeeper is a centralized service for maintaining configuration information, naming, providing distributed synchronization, and providing group services.
+ * [Kafka-Node] - Kafka-node is a Node.js client with Zookeeper integration for Apache Kafka 0.8.1 and later. 
+[Kafka]:http://kafka.apache.org/
+[Zookeeper]:https://zookeeper.apache.org/
+[NodeRED]:http://nodered.org
+[Kafka-Node]:https://www.npmjs.com/package/kafka-node
+
+
+# Authors
+
+* Feng Wang - [fwang1@us.ibm.com](mailto:fwang1@us.ibm.com)
---
+++ kafka.js
@@ -0,0 +1,124 @@
+/**
+ * Created by fwang1 on 3/25/15.
+ */
+module.exports = function(RED) {
+    /*
+     *   Kafka Producer
+     *   Parameters:
+     - topics 
+     - zkquorum(example: zkquorum = “[host]:2181")
+     */
+    function kafkaNode(config) {
+        RED.nodes.createNode(this,config);
+        this.topic = config.topic;
+        this.clusterZookeeper = config.zkquorum;
+        var node = this;
+        var kafka = require('kafka-node');
+        var HighLevelProducer = kafka.HighLevelProducer;
+        var Client = kafka.Client;
+        var topics = config.topics;
+        var client = new Client(clusterZookeeper);
+
+        try {
+            this.on("input", function(msg) {
+                var payloads = [];
+
+                // check if multiple topics
+                if (topics.indexOf(",") > -1){
+                    var topicArry = topics.split(',');
+
+                    for (i = 0; i < topicArry.length; i++) {
+                        payloads.push({topic: topicArry[i], messages: msg.payload});
+                    }
+                }
+                else {
+                    payloads = [{topic: topics, messages: msg.payload}];
+                }
+
+                producer.send(payloads, function(err, data){
+                    if (err){
+                        node.error(err);
+                    }
+                    node.log("Message Sent: " + data);
+                });
+            });
+        }
+        catch(e) {
+            node.error(e);
+        }
+        var producer = new HighLevelProducer(client);
+    }
+
+    RED.nodes.registerType("kafka",kafkaNode);
+
+
+    /*
+     *   Kafka Consumer
+     *   Parameters:
+     - topics
+     - groupId
+     - zkquorum(example: zkquorum = “[host]:2181")
+     */
+    function kafkaInNode(config) {
+        RED.nodes.createNode(this,config);
+
+        var node = this;
+
+        var kafka = require('kafka-node');
+        var HighLevelConsumer = kafka.HighLevelConsumer;
+        var Client = kafka.Client;
+        var topics = String(config.topics);
+        var clusterZookeeper = config.zkquorum;
+        var groupId = config.groupId;
+        var client = new Client(clusterZookeeper);
+
+        var topicJSONArry = [];
+
+        // check if multiple topics
+        if (topics.indexOf(",") > -1){
+            var topicArry = topics.split(',');
+            console.log(topicArry)
+            console.log(topicArry.length);
+
+
+            for (i = 0; i < topicArry.length; i++) {
+                console.log(topicArry[i]);
+                topicJSONArry.push({topic: topicArry[i]});
+            }
+            topics = topicJSONArry;
+        }
+        else {
+            topics = [{topic:topics}];
+        }
+
+        var options = {
+            groupId: groupId,
+            autoCommit: false,
+            autoCommitMsgCount: 10
+        };
+
+
+        try {
+            var consumer = new HighLevelConsumer(client, topics, options);
+            this.log("Consumer created...");
+
+            consumer.on('message', function (message) {
+                console.log(message);
+                node.log(message);
+                var msg = {payload: message};
+                node.send(msg);
+            });
+
+            
+            consumer.on('error', function (err) {
+               console.error(err);
+            });
+        }
+        catch(e){
+            node.error(e);
+            return;
+        }
+    }
+
+    RED.nodes.registerType("kafka in", kafkaInNode);
+}
